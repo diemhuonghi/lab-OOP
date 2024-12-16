1d96bb34da0761664287ae2cb62fb433281f9881
diemhuonghi <dymhuong2911@gmail.com>
2024-12-16 23:06:27 +0700

    lab

---
+++ LAB_05.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_05._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //khai báo và khởi tạo mảng 1 chiều
+            int[] m = { 2, 3, 4, 1, 5, 8, 7, 9, 6 };
+            //duyệt mảng và in ra dữ liệu
+            Console.Write(" Cac phan tu cua mang: ");
+
+        }
+    }
+}
